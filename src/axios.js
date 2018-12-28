73af5b9b38b7f0ab5d815c23bfa56e96f358b825
WuMat <wuzikm@gmail.com>
2018-12-28 11:53:06 +0100

    correct back url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://whispering-shore-72195.herokuapp.com'
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://whispering-shore-72195.herokuapp.com'
+  // baseURL: 'http://localhost:8080'
 })
 
 export default instance
